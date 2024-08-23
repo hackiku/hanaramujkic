5677bfba90a906dbb10d32ab2e611970e25e896a
hackiku <ivan@pipewriter.io>
2024-08-23 19:08:58 +0200

    menu button in navbar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,11 +2,8 @@
 
 <script lang="ts">
   import { onMount } from 'svelte';
-  import { Sun, Moon } from "lucide-svelte";
-  import { toggleMode } from "mode-watcher";
-  import { Button } from "$lib/components/ui/button";
   import Portfolio from "$lib/components/Portfolio.svelte";
-  import Menu from "$lib/components/Menu.svelte";
+  import Navbar from "$lib/components/Navbar.svelte";
 
   let projects = [];
 
@@ -17,21 +14,7 @@
   });
 </script>
 
-<nav class="flex justify-between items-center px-24 py-4">
-  <button class="text-xl font-semibold">HR</button>
-  <div class="flex items-center space-x-2">
-    <Button on:click={toggleMode} variant="outline" size="icon">
-      <Sun
-        class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-      />
-      <Moon
-        class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-      />
-      <span class="sr-only">Toggle theme</span>
-    </Button>
-    <Menu />
-  </div>
-</nav>
+<Navbar />
 
 <main class="px-24 py-8">
   <h1 class="text-6xl font-bold mb-4">SCENOGRAPHY</h1>
